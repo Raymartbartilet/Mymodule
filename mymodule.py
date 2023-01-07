22a87cfc3bc7ea205d1dc982041f01a23c744f12
Raymart Bartilet <114345498+Raymartbartilet@users.noreply.github.com>
2023-01-07 22:18:31 +0800

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,14 @@
+import math 
+
+sqrtvalue = math.pow(57.3, 7)
+print("sqrt: ", math.sqrt(3))
+print("pi: ", math.pi)
+print("tan: ", math.tan(6))
+print("cos: ", math.cos(5.4))
+print("floor: ", math.floor(7.3))
+print("fabs: ", math.fabs(46))
+print("ceil: ", math.ceil(60))
+print("sin: ", math.sin(80))
+print("exp: ", math.exp(79))
+print("log: ", math.log(27))
+print("log10: ", math.log10(1000))
